0a59229abe63211c7ad58f7f7842e3c846ac2618
veggieman420 <joshdoesstuff4fun@gmail.com>
2018-11-03 00:37:43 -0400

    added water atomizer

--- ECHacks2018.ino
+++ ECHacks2018.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
+// the setup function runs once when you press reset or power the board
+void setup() {  // initialize digital pin 13 as an output.
+  pinMode(A5, OUTPUT);// Set A5 as OUTPUT
+}  // the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  /* code */
+    digitalWrite(A5, HIGH);
+    // atomize  delay(5000);
+    // wait for 10 seconds
+    digitalWrite(A5, LOW);
+    // atomization stopped
+  
 }
+
+
+
