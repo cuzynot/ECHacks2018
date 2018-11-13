f00754d09d63d56c43f886b9c598f4e9720f449b
ma521 <yilistuffs510@gmail.com>
2018-11-12 23:15:31 -0500

    commenting on ino

--- Humidifier/Humidifier.ino
+++ Humidifier/Humidifier.ino
@@ -1,4 +1,4 @@
-#include "AirQuality.h"
+#include "AirQuality.h" // COMMENT OUT ALL SERIAL PRINTING IN .cpp FILE
 #include "Arduino.h"
 #include <SimpleDHT.h>
 
@@ -11,17 +11,21 @@ int pinDHT11 = 2;
 SimpleDHT11 dht11(pinDHT11);
 
 // strings
-String inputString = "";         // a string to hold incoming data
-String prevString = "";
+String inputString = ""; // a string to hold incoming data
 
 // bools
-boolean buttonClicked = false;  // whether the string is complete
+boolean buttonClicked = false; // whether the string is complete
 boolean slided = false;
-
-// int prevValue = 0;          // previous value from the potmeter
 
 // the desired humidity set by the user
 int userHumidity = 0;
+
+// init values
+byte temperature = 0;
+byte humidity = 0;
+
+// error value
+int err = SimpleDHTErrSuccess;
 
 void setup() {
   // initialize serial:
@@ -35,13 +39,15 @@ void setup() {
 }
 
 void loop() {
-  
-  Serial.print("1");
-  // Recieve data from Node and write it to a String
+  // reset string and slided values
+  inputString = "";
+  slided = false;
+
+  // Recieve data from JS and write it to a String
   for (int i = 0; i < 100; i++) {
     if (Serial.available() > 0) {
       char c = (char)Serial.read();
-      if (c == 'E') { // end character for led
+      if (c == 'E') { // end character for button
         buttonClicked = !buttonClicked;
         break;
       } else if (c == 'P') {
@@ -55,91 +61,52 @@ void loop() {
     delay(20);
   }
 
-  Serial.print("1");
-  
-  // get air quality
+  // RECEIVE VALUES
   aq = aqs.slope();
-  // delay(500);
-
-  Serial.print("2");
-
-  // get temp and humidity
-  byte temperature = 0;
-  byte humidity = 0;
-  int err = SimpleDHTErrSuccess;
-
-  Serial.print("3");
-
-  // if there's an error
   if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-//    Serial.print("Read DHT11 failed, err=");
-//    Serial.println(err);
+    //    Serial.print("Read DHT11 failed, err=");
+    //    Serial.println(err);
     delay(1000);
-    // return;
+    return;
   }
 
-  Serial.print("4");
-  
+  // if Myst is toggled on
   if (buttonClicked) {
-//    // print temperature
-//    Serial.print("temperature:");
-//    Serial.print((int)temperature);
-//    Serial.println(" *C, ");
-//
-//    // print humidity
-//    Serial.print("humidity:");
-//    Serial.print((int)humidity);
-//    Serial.println(" H");
-
     if ((int)humidity < userHumidity) {
+      // turn Myst on if cur humidity is lower than desired humidity
       digitalWrite(A5, HIGH);
     } else {
+      // turn Myst off if cur humidity exceeds desired humidity
       digitalWrite(A5, LOW);
     }
-//    Serial.println(userHumidity);
   } else {
+    // turn Myst off
     digitalWrite(A5, LOW);
   }
 
-  Serial.print("5");
-
-  inputString = "";
-  slided = false;
-
-  // sensorValue = analogRead(analogInPin);
-  //  Serial.print("sensor ");
-  //  Serial.println(sensorValue);
-
-  // testing
+  // TESTING
   temperature = random(-30, 40);
   humidity = random(30, 60);
   aq = random(0, 700);
 
+  // print temperature
   Serial.print("T"); // begin character
   Serial.print(temperature);
   Serial.print("P"); // end character
 
-  Serial.print("H"); // begin character
+  // print humidity
+  Serial.print("H");
   Serial.print(humidity);
-  Serial.print("M"); // end character
+  Serial.print("M");
 
-  Serial.print("A"); // begin character
+  // print air quality
+  Serial.print("A");
   Serial.print(aq);
-  Serial.print("Q"); // end character
+  Serial.print("Q");
 
   delay(2000);
   Serial.println();
-
-  Serial.print("6");
 }
-
-//int stringToInt() {
-//  char charHolder[inputString.length() + 1];
-//  inputString.toCharArray(charHolder, inputString.length() + 1);
-//  inputString = "";
-//  int _recievedVal = atoi(charHolder);
-//  return _recievedVal;
-//}
 
 ISR(TIMER2_OVF_vect) {
   if (aqs.counter == 122) { //set 2 seconds as a detected duty
